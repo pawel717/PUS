76251576b683d3b14723e8bb9d0f3dfdc593b3e4
pawel <psuchanicz@gmail.com>
2018-03-19 23:46:49 +0100

    Formatting fixed

--- lab1/zad5/server2_5/server.c
+++ lab1/zad5/server2_5/server.c
@@ -39,48 +39,47 @@ struct context
 
 int main(int argc, char *argv[ ])
 {
-    if (argc != 2)
-    {
-        fprintf(stderr, "Wywołanie: %s <PORT>\n", argv[0]);
-        system("pause");
-        exit(EXIT_FAILURE);
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "Wywołanie: %s <PORT>\n", argv[0]);
+		system("pause");
+		exit(EXIT_FAILURE);
+	}
 
 	signal(SIGINT, CloseServer);
 
 	page_HTML = createHTMLpage();
 
-    struct sockaddr_in server_addr;
+	struct sockaddr_in server_addr;
 
-    int server_port = atoi(argv[1]);
+	int server_port = atoi(argv[1]);
 
-    memset(&server_addr, 0, sizeof(server_addr));
+	memset(&server_addr, 0, sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(server_port);
+	server_addr.sin_port = htons(server_port);
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (server_fd == -1)
-    	Error("[SERWER]: Nie moglem utworzyc gniazda.\n");
-    else
-    	printf("[SERWER]: Gniazdo zostalo stworzone %d.\n",server_fd);
+	if (server_fd == -1)
+		Error("[SERWER]: Nie moglem utworzyc gniazda.\n");
+	else
+		printf("[SERWER]: Gniazdo zostalo stworzone %d.\n",server_fd);
 
-    if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) == -1)
+	if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) == -1)
 		Error("[SERWER]: Nie moge nadac nazwy gniazdu.\n");
-    else
-    	printf("[SERWER]: Gniazdo otrzymalo swoja nazwe.\n");
+	else
+		printf("[SERWER]: Gniazdo otrzymalo swoja nazwe.\n");
 
-    if (listen(server_fd, SOMAXCONN) == -1)
+	if (listen(server_fd, SOMAXCONN) == -1)
 		Error("[SERWER]: Nie moge utworzy kolejki.\n");
-    else
-    	printf("[SERWER]: Kolejka zostala utworzona.\n");
+	else
+		printf("[SERWER]: Kolejka zostala utworzona.\n");
 
 	printf("[SERWER]: Oczekuje na klientów...\n");
 
 	while (1)
 	{
-
 		pthread_t thread_id;
 
 		struct context context;
@@ -89,7 +88,6 @@ int main(int argc, char *argv[ ])
 
 		if (pthread_create(&thread_id, NULL, &handleHTTPRequest, (void*)&context) == -1)
 			Error("[SERWER]: Błąd! Nie utowrzono wątku...\n");
-
 	}
 }
 
@@ -110,7 +108,6 @@ int checkNewConnection()
 
 void* handleHTTPRequest(void * arg)
 {
-
 	struct context * context = (struct context *) arg;
 
 	char request[2000];
@@ -120,22 +117,19 @@ void* handleHTTPRequest(void * arg)
 	received_bytes = recv(server_fd, &request, sizeof(request), 0);
 	//check if it is proper http request
 
-	//if url starts with /img/ and ends with .jpeg, .jpg, .png, .gif
+	//if GET demands image -> url starts with /img/ and ends with .jpeg, .jpg, .png, .gif?
 
 	//send image as respone
 
-	//else send page_HTML
-
+	//else send header+page_HTML
 
 	send(context->client, page_HTML, sizeof(page_HTML));
 
 	return 0;
-
 }
 
 char * createHTMLpage()
 {
-
 	char * page_HTML;
 	DIR * directory;
 	struct dirent * image;
@@ -155,22 +149,17 @@ char * createHTMLpage()
 	strcat(page_HTML, "</center></body></html> ");
 
 	return page_HTML;
-
 }
 
 void CloseServer()
 {
-
 	printf("[SERWER] Zamykanie serwera...\n");
-   	close(server_fd);
-  	exit(0);
-
+	close(server_fd);
+	exit(0);
 }
 
 static void Error(char desc[])
 {
-
 	printf("%s\n", desc);
 	exit(EXIT_FAILURE);
-
 }
