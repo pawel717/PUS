f308d655c6e5fee8c76b6d00e4f19b6aa2d80f9a
Lukas <lsujk95@gmail.com>
2018-04-18 10:46:44 +0200

    Zad3

---
+++ lab4/zad3/pus_lab4_3_client/client.c
@@ -0,0 +1,134 @@
+/*
+ * client.c
+ *
+ *  Created on: 15 kwi 2018
+ *      Author: root
+ */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <sys/un.h>
+#include <sys/ipc.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <errno.h>
+#include <netdb.h>
+#include <netinet/sctp.h>
+
+static void Error(char desc[]);
+void CloseClient();
+
+int sock_fd;
+
+int main(int argc, char* argv[])
+{
+
+	if(argc != 3)
+	{
+		 fprintf(stderr, "Wywołanie: %s <IPv6 ADDRESS> <PORT>\n", argv[0]);
+		 system("pause");
+		 exit(EXIT_FAILURE);
+	}
+
+	signal(SIGINT, &CloseClient);
+
+	int server_port = atoi(argv[2]);
+	char* server_address = argv[1];
+
+	struct addrinfo address_info;
+    struct addrinfo *result_addresses;
+
+    struct sctp_sndrcvinfo sndrcvinfo;
+
+    struct sctp_status status;
+    int status_len = sizeof(status);
+
+    struct sctp_initmsg initmsg;
+    initmsg.sinit_max_attempts = 5;
+    initmsg.sinit_max_instreams = 1;
+    initmsg.sinit_num_ostreams = 4;
+
+    memset(&address_info, 0, sizeof(struct addrinfo));
+    address_info.ai_family = AF_UNSPEC;
+    address_info.ai_socktype = SOCK_STREAM;
+    address_info.ai_flags = 0;
+    address_info.ai_protocol = IPPROTO_SCTP;
+
+    if(getaddrinfo(argv[1], argv[2],  &address_info, &result_addresses) == -1)
+    {
+		fprintf(stderr, "getaddrinfo() failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    struct addrinfo *iterator;
+    for(iterator=result_addresses; iterator!=NULL; iterator = iterator->ai_next)
+    {
+    	sock_fd = socket(iterator->ai_family, iterator->ai_socktype, iterator->ai_protocol);
+
+    	if(sock_fd != -1)
+    	{
+    	    if(setsockopt(sock_fd, IPPROTO_SCTP, SCTP_INITMSG, &initmsg, sizeof (initmsg)) != 0)
+    	    	Error("[KLIENT]: Błąd przy ustawianiu opcji SCTP_INITMSG");
+
+    		if(connect(sock_fd, iterator->ai_addr, iterator->ai_addrlen) != -1)
+    			break;
+    	}
+
+    	close(sock_fd);
+    }
+
+	if (iterator == NULL)
+		Error("[KLIENT]: Nie moglem utworzyc gniazda.\n");
+
+	freeaddrinfo(result_addresses);
+
+	struct sctp_event_subscribe s_events;
+
+    memset (&s_events, 0, sizeof (s_events));
+    s_events.sctp_data_io_event = 1;
+    setsockopt (sock_fd, SOL_SCTP, SCTP_EVENTS,(const void *) &s_events, sizeof (s_events));
+
+	// print association id, association state and number of in and out streams getsockopt(SCTP_STATUS)
+    getsockopt(sock_fd, SOL_SCTP, SCTP_STATUS,(void *) &status, (socklen_t *) &status_len);
+
+	printf ("[KLIENT]: Id asocjacji %d\n", status.sstat_assoc_id);
+	printf ("[KLIENT]: Stan asocjacji: %d\n", status.sstat_state);
+	printf ("[KLIENT]: Ilość strumieni wychodzacych: %d\n", status.sstat_outstrms);
+	printf ("[KLIENT]: Ilość strumieni przychodzacych: %d\n", status.sstat_instrms);
+
+	// receive date and time from different streams
+	int i;
+    for (i = 0; i <2; i++)
+    {
+    	char message[256];
+    	memset(message, '\0', sizeof(message) );
+
+        int retval = sctp_recvmsg(sock_fd, (void *) message, sizeof(message),(struct sockaddr *) NULL, 0, &sndrcvinfo, NULL);
+
+        if (retval > 0)
+            printf ("[KLIENT]: Strumień %d odebrano: %s\n", sndrcvinfo.sinfo_stream, message);
+
+    }
+
+	// then terminate
+
+	CloseClient();
+	return 0;
+}
+
+static void Error(char desc[])
+{
+	printf("%s", desc);
+	printf("%d", errno);
+	exit(EXIT_FAILURE);
+}
+
+void CloseClient()
+{
+	printf("[KLIENT] Zamykanie klienta...\n");
+   	close(sock_fd);
+  	exit(0);
+}
---
+++ lab4/zad3/pus_lab4_3_server/server.c
@@ -0,0 +1,132 @@
+/*
+ * server.c
+ *
+ *  Created on: 15 kwi 2018
+ *      Author: root
+ */
+
+#include <sys/socket.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <string.h>
+#include <time.h>
+#include <netinet/sctp.h>
+
+void CloseServer();
+static void Error(char desc[]);
+
+int sock_fd;
+int client_fd;
+
+int main(int argc, char* argv[])
+{
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "Wywołanie: %s <PORT>\n", argv[0]);
+        system("pause");
+        exit(EXIT_FAILURE);
+    }
+
+	signal(SIGINT, CloseServer);
+
+	struct sctp_initmsg initmsg;
+
+    memset (&initmsg, 0, sizeof (initmsg));
+    initmsg.sinit_num_ostreams = 2;
+    initmsg.sinit_max_instreams = 2;
+
+    struct sockaddr_in server_addr;
+
+    int server_port = atoi(argv[1]);
+
+    memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(server_port);
+
+    sock_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_SCTP);
+
+    if (sock_fd == -1)
+    	Error("[SERWER]: Nie moglem utworzyc gniazda.\n");
+    else
+    	printf("[SERWER]: Gniazdo zostalo stworzone %d.\n",sock_fd);
+
+    if (bind(sock_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) == -1)
+		Error("[SERWER]: Nie moge nadac nazwy gniazdu.\n");
+    else
+    	printf("[SERWER]: Gniazdo otrzymalo swoja nazwe.\n");
+
+    if(setsockopt(sock_fd, IPPROTO_SCTP, SCTP_INITMSG, &initmsg, sizeof (initmsg)) != 0)
+		Error("[SERWER]: Błąd przy ustawianiu opcji SCTP_INITMSG");
+
+
+    if (listen(sock_fd, SOMAXCONN) == -1)
+		Error("[SERWER]: Nie moge utworzy kolejki.\n");
+    else
+    	printf("[SERWER]: Kolejka zostala utworzona.\n");
+
+	printf("[SERWER]: Oczekuje na klientów...\n");
+
+	while(1)
+	{
+
+		char src[INET_ADDRSTRLEN];
+		struct sockaddr_in client_addr;
+		memset(&client_addr, 0, sizeof(client_addr));
+	    int client_length = sizeof(client_addr);
+
+		client_fd = accept(sock_fd, (struct sockaddr *)&client_addr, &client_length);
+
+		if(client_fd == -1)
+			Error("[SERWER]: Nie udało się zaakceptować połaczenia");
+		else
+			printf("[SERWER]: Nowe połaczenie od klienta (adres: %s:%d)\n",
+					inet_ntop(client_addr.sin_family, &client_addr, src, sizeof(src)),
+					client_addr.sin_port);
+
+		char message[256];
+		memset(message, '\0', sizeof(message));
+
+		time_t t = time(NULL);
+		struct tm current_time = *localtime(&t);
+
+		sprintf(message, "Data: %d/%d/%d", current_time.tm_mday, current_time.tm_mon+1, current_time.tm_year+1900);
+
+		if(sctp_sendmsg(client_fd, message, strlen(message), NULL, 0, 0, 0, 0, 0, 0) == -1)
+			Error("[SERWER]: Błąd wysyłania");
+
+		printf("[SERWER]: Wysłano date\n");
+
+		memset(message, '\0', sizeof(message));
+		sprintf(message, "Czas: %d:%d:%d", current_time.tm_hour, current_time.tm_min, current_time.tm_sec);
+
+		if(sctp_sendmsg(client_fd, message, strlen(message), NULL, 0, 0, 0, 1, 0, 0) == -1)
+			Error("[SERWER]: Błąd wysyłania");
+
+		printf("[SERWER]: Wysłano czas\n");
+
+	}
+
+	CloseServer();
+
+}
+
+void CloseServer()
+{
+	printf("[SERWER] Zamykanie serwera...\n");
+   	close(sock_fd);
+   	close(client_fd);
+  	exit(0);
+}
+
+static void Error(char desc[])
+{
+	printf("%s\n", desc);
+	perror(NULL);
+	exit(EXIT_FAILURE);
+}
